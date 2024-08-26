4e7eabd7969169e0c5201d1020d283509bf674bc
Sofialv05 <sofalvsy@gmail.com>
2024-08-26 10:16:26 +0700

    fix:navbar

--- todolist-app/components/Navbar.vue
+++ todolist-app/components/Navbar.vue
@@ -1,13 +1,5 @@
 <template>
   <div class="flex h-16 items-center border-b border-gray-200 bg-white py-4">
     <HamburgerButton />
-
-    <div class="ml-auto pr-4">
-      <button
-        class="flex items-center rounded-md p-2 font-bold text-red-500 hover:bg-gray-300 focus:text-gray-700 focus:outline-none"
-      >
-        <i class="pi pi-sign-out"></i>
-      </button>
-    </div>
   </div>
 </template>
